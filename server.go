8c0c4ea4f0ac3e253a51c999ee2852115f30d4b4
Luke Oleson <lukeoleson@gmail.com>
2021-08-22 19:42:49 -0700

    mysql db setup

--- server.go
+++ server.go
@@ -5,8 +5,11 @@ import (
 	"net/http"
 	"os"
 
+	database "github.com/lukeoleson/graphql-gettingstarted/internal/pkg/db/mysql"
+
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi/v5"
 	"github.com/lukeoleson/graphql-gettingstarted/graph"
 	"github.com/lukeoleson/graphql-gettingstarted/graph/generated"
 )
@@ -19,11 +22,14 @@ func main() {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	router := chi.NewRouter()
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	database.InitDB()
+	database.Migrate()
+	server := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", server)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
